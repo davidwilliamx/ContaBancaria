e3fe659672924ceaa0b41f79e4186487409bc12f
David Magalhães <david.williamx@gmail.com>
2024-05-10 12:54:50 -0300

    Altera utilização do switch para if else.

--- src/Main.java
+++ src/Main.java
@@ -21,47 +21,41 @@ public class Main {
             System.out.println(menu);
             opcaoMenu = entrada.nextInt();
 
-            switch (opcaoMenu) {
-                case 1:
-                    System.out.println("Consultar saldos");
-                    System.out.println("*****************************");
-                    System.out.println("Cliente: " + nome);
-                    System.out.println("Tipo de conta: " + tipoConta);
-                    System.out.println("Seu saldo é: " + saldo);
-                    System.out.println("*****************************");
-                    break;
-                case 2:
-                    System.out.println("Retirar valor");
-                    System.out.println("Informe o valor que deseja retirar: ");
-                    double valorRetirada = entrada.nextDouble();
-                    if (valorRetirada > saldo) {
-                        System.out.println("O valor informado ultrapassa o seu saldo!");
-                        System.out.println("Informe um valor menor: ");
-                        valorRetirada = entrada.nextDouble();
-                        saldo -= valorRetirada;
-                        System.out.println("Seu novo saldo é: " + saldo);
-                    } else {
-                        saldo -= entrada.nextDouble();
-                        System.out.println("Seu novo saldo é: " + saldo);
-                    }
-                    break;
-                case 3:
-                    System.out.println("Depositar valor");
-                    System.out.println("Informe o valor que deseja depositar: ");
-                    double valorDeposito = entrada.nextDouble();
-                    saldo += valorDeposito;
+            if (opcaoMenu == 1) {
+                System.out.println("Consulta Saldo");
+                System.out.println("*****************************");
+                System.out.println("Cliente: " + nome);
+                System.out.println("Tipo de conta: " + tipoConta);
+                System.out.println("Seu saldo atualizado é: " + saldo);
+                System.out.println("*****************************");
+            } else if (opcaoMenu == 2) {
+                System.out.println("Retirar valor");
+                System.out.println("Informe o valor que deseja retirar: ");
+                double valorRetirada = entrada.nextDouble();
+                if (valorRetirada > saldo) {
+                    System.out.println("O valor informado ultrapassa o seu saldo!");
+                    System.out.println("Informe um valor menor: ");
+                    valorRetirada = entrada.nextDouble();
+                    saldo -= valorRetirada;
                     System.out.println("Seu novo saldo é: " + saldo);
-                    break;
-                case 4:
-                    System.out.println("Sair");
-                    System.out.println("Até mais!");
-                    break;
-                default:
-                    System.out.println("Opção inválida");
-                    System.out.println("Tente novamente!");
-                    opcaoMenu = entrada.nextInt();
-                    break;
+                } else {
+                    saldo -= entrada.nextDouble();
+                    System.out.println("Seu novo saldo é: " + saldo);
+                }
+            } else if (opcaoMenu == 3) {
+                System.out.println("Depositar valor");
+                System.out.println("Informe o valor que deseja depositar: ");
+                double valorDeposito = entrada.nextDouble();
+                saldo += valorDeposito;
+                System.out.println("Seu novo saldo é: " + saldo);
+            } else if (opcaoMenu == 4) {
+                System.out.println("Sair");
+                System.out.println("Até mais!");
+            } else {
+                System.out.println("Opção inválida");
+                System.out.println("Tente novamente!");
+                opcaoMenu = entrada.nextInt();
+            }
             }
         }
     }
-}
