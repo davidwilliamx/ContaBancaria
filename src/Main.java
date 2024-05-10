9b940437641a224f8453367890e1b39932a30fd0
David Magalhães <david.williamx@gmail.com>
2024-05-10 12:13:25 -0300

    Altera exibição do menu utilizando Text Box, cria um loop while para o menu.

--- src/Main.java
+++ src/Main.java
@@ -7,55 +7,61 @@ public class Main {
         String nome = "David Magalhães";
         String tipoConta = "Conta Corrente";
         double saldo = 1500.0;
+        int opcaoMenu = 0;
 
-        System.out.println("Seja bem-vindo(a), " + nome + "!");
-        System.out.println("Selecione uma opção: ");
-        System.out.println("1. Consultar saldo");
-        System.out.println("2. Retirar valor");
-        System.out.println("3. Despositar valor");
-        System.out.println("4. Sair");
-        int opcaoMenu = entrada.nextInt();
+        String menu = """
+                Digite uma opção:
+                1. Consultar saldo
+                2. Retirar valor
+                3. Despositar valor
+                4. Sair
+                """;
 
-        switch (opcaoMenu) {
-            case 1:
-                System.out.println("Consultar saldos");
-                System.out.println("*****************************");
-                System.out.println("Cliente: " + nome);
-                System.out.println("Tipo de conta: " + tipoConta);
-                System.out.println("Seu saldo é: " + saldo);
-                System.out.println("*****************************");
-                break;
-            case 2:
-                System.out.println("Retirar valor");
-                System.out.println("Informe o valor que deseja retirar: ");
-                double valorRetirada = entrada.nextDouble();
-                if (valorRetirada > saldo) {
-                    System.out.println("O valor informado ultrapassa o seu saldo!");
-                    System.out.println("Informe um valor menor: ");
-                    valorRetirada = entrada.nextDouble();
-                    saldo -= valorRetirada;
+        while (opcaoMenu != 4) {
+            System.out.println(menu);
+            opcaoMenu = entrada.nextInt();
+
+            switch (opcaoMenu) {
+                case 1:
+                    System.out.println("Consultar saldos");
+                    System.out.println("*****************************");
+                    System.out.println("Cliente: " + nome);
+                    System.out.println("Tipo de conta: " + tipoConta);
+                    System.out.println("Seu saldo é: " + saldo);
+                    System.out.println("*****************************");
+                    break;
+                case 2:
+                    System.out.println("Retirar valor");
+                    System.out.println("Informe o valor que deseja retirar: ");
+                    double valorRetirada = entrada.nextDouble();
+                    if (valorRetirada > saldo) {
+                        System.out.println("O valor informado ultrapassa o seu saldo!");
+                        System.out.println("Informe um valor menor: ");
+                        valorRetirada = entrada.nextDouble();
+                        saldo -= valorRetirada;
+                        System.out.println("Seu novo saldo é: " + saldo);
+                    } else {
+                        saldo -= entrada.nextDouble();
+                        System.out.println("Seu novo saldo é: " + saldo);
+                    }
+                    break;
+                case 3:
+                    System.out.println("Depositar valor");
+                    System.out.println("Informe o valor que deseja depositar: ");
+                    double valorDeposito = entrada.nextDouble();
+                    saldo += valorDeposito;
                     System.out.println("Seu novo saldo é: " + saldo);
-                } else {
-                    saldo -= entrada.nextDouble();
-                    System.out.println("Seu novo saldo é: " + saldo);
-                }
-                break;
-            case 3:
-                System.out.println("Depositar valor");
-                System.out.println("Informe o valor que deseja depositar: ");
-                double valorDeposito = entrada.nextDouble();
-                saldo += valorDeposito;
-                System.out.println("Seu novo saldo é: " + saldo);
-                break;
-            case 4:
-                System.out.println("Sair");
-                System.out.println("Até mais!");
-                break;
-            default:
-                System.out.println("Opção inválida");
-                System.out.println("Tente novamente!");
-                opcaoMenu = entrada.nextInt();
-                break;
+                    break;
+                case 4:
+                    System.out.println("Sair");
+                    System.out.println("Até mais!");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+                    System.out.println("Tente novamente!");
+                    opcaoMenu = entrada.nextInt();
+                    break;
+            }
         }
     }
 }
